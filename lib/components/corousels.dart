43d0a5f0689a15a15689627eb9f3d73e7a05aa53
Shukla Manan D. <mananshukla1512@gmail.com>
2024-02-16 15:28:37 +0530

    Popular news on Corousel and also taps for more see about it.

--- lib/components/corousels.dart
+++ lib/components/corousels.dart
@@ -1,21 +1,35 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:news_app/components/bottomsheet.dart';
 
 class VerticalSliderDemo extends StatelessWidget {
-  final List imgslide;
-  const VerticalSliderDemo({super.key, required this.imgslide});
+  final List<Widget> imgslide;
+  final List url, imgurl, description, text;
+  const VerticalSliderDemo(
+      {super.key,
+      required this.imgslide,
+      required this.url,
+      required this.imgurl,
+      required this.description,
+      required this.text});
 
   @override
   Widget build(BuildContext context) {
-    return CarouselSlider(
-      options: CarouselOptions(
-        aspectRatio: 2.0,
-        enlargeCenterPage: true,
-        scrollDirection: Axis.horizontal,
-        autoPlay: true,
+    return GestureDetector(
+      onTap: () {
+        print("hi-------------------------");
+        // MyBottomsheetlayout(title: text, description: description, imageurl: imageurl, url: url)
+      },
+      child: CarouselSlider(
+        options: CarouselOptions(
+          aspectRatio: 2.0,
+          enlargeCenterPage: true,
+          scrollDirection: Axis.horizontal,
+          autoPlay: true,
+        ),
+        items: imgslide,
+        // items: imageSliders,
       ),
-      items: [],
-      // items: imageSliders,
     );
   }
 }
--- lib/pages/Home.dart
+++ lib/pages/Home.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:liquid_pull_to_refresh/liquid_pull_to_refresh.dart';
 import 'package:news_app/backend/functions.dart';
+import 'package:news_app/components/bottomsheet.dart';
+import 'package:news_app/components/componentes1.dart';
 import 'package:news_app/components/corousels.dart';
 // import 'package:news_app/components/appbar.dart';
 import 'package:news_app/components/searchbar.dart';
@@ -20,13 +23,19 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   late Future<List> news;
-  List<String> newsimg = [""];
+  List<Widget> newsimg = [];
+  List text = [], description = [], imgurl = [], url = [];
   String category = "";
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     news = fetchnews("");
+  }
+
+  Future<void> refresh() async {
+    setState(() {});
+    return await Future.delayed(const Duration(milliseconds: 10));
   }
 
   @override
@@ -57,72 +66,144 @@ class _HomeState extends State<Home> {
         ),
         centerTitle: true,
       ),
-      body: Column(
-        children: [
-          const SerchBar(),
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              children: [
-                filters(text: 'Business', f: f),
-                filters(text: 'Entertainment', f: f),
-                filters(text: 'General', f: f),
-                filters(text: 'Health', f: f),
-                filters(text: 'Technology', f: f),
-                filters(text: 'Science', f: f),
-                filters(text: 'Sports', f: f),
-              ],
-            ),
-          ),
-          SizedBox(
-            width: w,
-            height: 10,
-          ),
-          VerticalSliderDemo(
-            imgslide: newsimg,
-          ),
-          Expanded(
-            child: Container(
-              width: w,
-              child: Consumer<changestring>(
-                builder: (context, value, child) {
-                  return FutureBuilder<List>(
-                    future: fetchnews(value.cat),
-                    builder: (context, snapshot) {
-                      if (snapshot.hasData) {
-                        for (int i = 0; i < snapshot.data!.length; i++) {
-                          newsimg.add(snapshot.data![i]['urlToImage'] ?? "");
-                          print(snapshot.data![i]['urlToImage']);
-                        }
-                        return ListView.builder(
-                            itemCount: snapshot.data!.length,
-                            itemBuilder: (context, index) {
-                              return newsbox(
-                                  url: snapshot.data![index]['url'],
-                                  imageurl: snapshot.data![index]
-                                          ['urlToImage'] ??
-                                      constants.imageurl,
-                                  title: snapshot.data![index]['title'],
-                                  time: snapshot.data![index]['publishedAt'],
-                                  description: snapshot.data![index]
-                                          ['description']
-                                      .toString());
-                            });
-                      } else if (snapshot.hasError) {
-                        return Text("${snapshot.error}");
-                      }
-                      return Center(
-                        child: CircularProgressIndicator(
-                          color: appcolor.primary,
-                        ),
-                      );
-                    },
-                  );
-                },
+      body: LiquidPullToRefresh(
+        backgroundColor: appcolor.black,
+        animSpeedFactor: 5.0,
+        onRefresh: () async {
+          setState(() {});
+          return await Future.delayed(Duration(milliseconds: 10));
+        },
+        child: Column(
+          children: [
+            const SerchBar(),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  filters(text: 'Business', f: f),
+                  filters(text: 'Entertainment', f: f),
+                  filters(text: 'General', f: f),
+                  filters(text: 'Health', f: f),
+                  filters(text: 'Technology', f: f),
+                  filters(text: 'Science', f: f),
+                  filters(text: 'Sports', f: f),
+                ],
               ),
             ),
-          )
-        ],
+            SizedBox(
+              width: w,
+              height: 15,
+            ),
+            VerticalSliderDemo(
+              imgslide: newsimg,
+              description: [],
+              imgurl: [],
+              text: [],
+              url: [],
+            ),
+            Expanded(
+              child: Container(
+                width: w,
+                child: Consumer<changestring>(
+                  builder: (context, value, child) {
+                    return FutureBuilder<List>(
+                      future: fetchnews(value.cat),
+                      builder: (context, snapshot) {
+                        if (snapshot.hasData) {
+                          newsimg.clear();
+                          text.clear();
+                          url.clear();
+                          description.clear();
+                          imgurl.clear();
+                          for (int i = 0; i < snapshot.data!.length; i++) {
+                            newsimg.add(
+                              InkWell(
+                                onTap: () {
+                                  showmybottomsheet(
+                                      context,
+                                      snapshot.data![i]['title'],
+                                      snapshot.data![i]['description'],
+                                      snapshot.data![i]['urlToImage'],
+                                      snapshot.data![i]['url']);
+                                },
+                                child: Container(
+                                  height: 200,
+                                  child: Stack(
+                                    children: [
+                                      Container(
+                                        foregroundDecoration:
+                                            const BoxDecoration(
+                                          gradient: LinearGradient(
+                                              colors: [
+                                                Colors.black,
+                                                Colors.transparent
+                                              ],
+                                              begin: Alignment.bottomCenter,
+                                              end: Alignment.topCenter),
+                                        ),
+                                        decoration: BoxDecoration(
+                                            borderRadius:
+                                                const BorderRadius.only(
+                                              topLeft: Radius.circular(20),
+                                              topRight: Radius.circular(20),
+                                            ),
+                                            image: DecorationImage(
+                                                image: NetworkImage(
+                                                    snapshot.data![i]
+                                                            ['urlToImage'] ??
+                                                        constants.imageurl),
+                                                fit: BoxFit.cover)),
+                                      ),
+                                      Positioned(
+                                        bottom: 10,
+                                        child: Container(
+                                          padding: EdgeInsets.all(10),
+                                          width: 300,
+                                          child: boldtext(
+                                              text: snapshot.data![i]['title'],
+                                              color: Colors.white,
+                                              size: 15),
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            );
+                            //newsimg.add(snapshot.data![i]['urlToImage'] ?? "");
+                            print(snapshot.data![i]['urlToImage']);
+                          }
+                          return ListView.builder(
+                              physics: const AlwaysScrollableScrollPhysics(),
+                              itemCount: snapshot.data!.length,
+                              itemBuilder: (context, index) {
+                                return newsbox(
+                                    url: snapshot.data![index]['url'],
+                                    imageurl: snapshot.data![index]
+                                            ['urlToImage'] ??
+                                        constants.imageurl,
+                                    title: snapshot.data![index]['title'],
+                                    time: snapshot.data![index]['publishedAt'],
+                                    description: snapshot.data![index]
+                                            ['description']
+                                        .toString());
+                              });
+                        } else if (snapshot.hasError) {
+                          return Text("${snapshot.error}");
+                        }
+                        return Center(
+                          child: CircularProgressIndicator(
+                            color: appcolor.primary,
+                          ),
+                        );
+                      },
+                    );
+                  },
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
